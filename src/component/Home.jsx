e474eb03808f9102eac3d205267e9dd9ad5476e9
angwanghyeon <agh1594@naver.com>
2023-12-19 00:00:47 +0900

    리액트로 프로젝트 변경

---
+++ src/component/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/Home.css'
+
+const Home = () => {
+    return (
+        <div className='container'>
+            <nav className='nav-bar'>
+                <img src="image/logo.png" alt="소리노을-로고" className='logo'/>
+            </nav>
+            
+        </div>
+    );
+};
+
+export default Home;
